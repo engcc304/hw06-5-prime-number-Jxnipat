2e6dd33211067f1c926cd1403d2626805fde7b5a
jxnipat <Jetnipat.prc@gmail.com>
2023-08-14 17:19:37 +0700

    up

--- Homework/HW06/hw06-5-prime-number.cpp
+++ Homework/HW06/hw06-5-prime-number.cpp
@@ -14,3 +14,34 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+int main() {
+    int input;
+
+    printf("Enter number:\n");
+    scanf("%d", &input);
+
+    printf("Output:\n");
+    for (int i = input - 1; i >= 2; --i) {
+        if (is_prime(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
